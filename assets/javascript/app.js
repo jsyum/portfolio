e160b3063ed84c652d8f59dd619b701e357dc0ec
jsyum <jennifersyum@gmail.com>
2019-03-17 02:01:05 -0400

    added portfolio images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,9 @@
 $("#splashpage").append("<div id=clickme>Click me</div>");
 
-$("#changepage").on("click", () => {
-  $("body").attr(
-    "style",
-    "background-image: url('assets/images/laptop-plant.jpg')"
-  );
-  $("#changepage").remove();
+$("#clickme").on("click", () => {
+  console.log("Clicked");
+  window.location = "#about";
+  //   scrollIntoView(true);
 });
 
 // $("#changepage").on("click", () => {
